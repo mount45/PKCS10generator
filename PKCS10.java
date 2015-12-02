2aec3523007dc0357d53e64b65541482e49ae770
Philip Ratcliffe <phil@philratcliffe.co.uk>
2015-12-02 14:21:55 +0000

    Updates to print out generated CSR in PEM format.

--- PKCS10.java
+++ PKCS10.java
@@ -2,6 +2,7 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.io.OutputStreamWriter;
 
 import javax.security.auth.x500.X500Principal;
 
@@ -9,12 +10,15 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 
 
 /**
- * A class that generates PKCS10 certificate signing requests
+ * A class that generates PKCS10 certificate signing requests for testing
+ * purposes only.
  * 
- * Need the bouncycaslte jars on CLASSPATH:
+ * The code requires that the bouncycaslte jars are on the CLASSPATH:
+ *
  * For example:
  *     copy bcprov-ext-jdk15on-153.jar and bcprov-jdk15on-153.jar to ~/jars
  *     export CLASSPATH=.:$CLASSPATH:~/jars/*
@@ -32,9 +36,16 @@ public class PKCS10 {
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(RSA_KEYSIZE, new SecureRandom());
         KeyPair kp = keyGen.genKeyPair();
+
+        // Generate the PKCS#10 CSR 
         JcaPKCS10CertificationRequestBuilder builder = new JcaPKCS10CertificationRequestBuilder(new X500Principal("cn=phil"), kp.getPublic());
         JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder("SHA1withRSA");
-        builder.build(signerBuilder.build(kp.getPrivate()));
+        PKCS10CertificationRequest csr = builder.build(signerBuilder.build(kp.getPrivate()));
+
+        OutputStreamWriter output = new OutputStreamWriter(System.out);
+        JcaPEMWriter pem = new JcaPEMWriter(output);
+        pem.writeObject(csr);
+        pem.close();
     }
  
 }
