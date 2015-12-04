0b6d79138e39e4142da39201c55af65a00862c30
Philip Ratcliffe <phil@philratcliffe.co.uk>
2015-12-04 15:30:34 +0000

    Added first cut of a C# version. Note this is far from complete.

---
+++ PKCS10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Security;
+
+namespace pkcs10gen
+{
+    class Program
+    {
+        public static AsymmetricCipherKeyPair MakeKeyPair()
+        {
+            RsaKeyPairGenerator rsaGen = new RsaKeyPairGenerator();
+
+            rsaGen.Init(new KeyGenerationParameters(new SecureRandom(), 1024));
+            AsymmetricCipherKeyPair keyPair = rsaGen.GenerateKeyPair();
+
+            return keyPair;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
