386f5117cba808ebf7cbd8e0aff2cc54c3a6b4e8
Philip Ratcliffe <phil@philratcliffe.co.uk>
2015-12-07 19:35:07 +0000

    First cut of python version.

---
+++ PKCS10.py
@@ -0,0 +1,13 @@
+from OpenSSL import crypto
+
+def generate_key_pair():
+    '''Generate a public/private RSA key pair'''
+    key_pair = crypto.PKey()
+    key_pair.generate_key(crypto.TYPE_RSA, 2048)
+    return key_pair
+
+key_pair = generate_key_pair()
+csr = crypto.X509Req()
+csr.get_subject().CN = "Phil Ratcliffe"
+csr.set_pubkey(key_pair)
+
